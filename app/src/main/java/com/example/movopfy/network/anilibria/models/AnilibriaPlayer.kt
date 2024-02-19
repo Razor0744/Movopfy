bd0da36403a908243ca101f535930b6d5380f293
siarheihaurylau <sgavrilov387@gmail.com>
2024-02-19 15:03:54 +0300

    map json to data class: fixed

--- app/src/main/java/com/example/movopfy/network/anilibria/models/AnilibriaPlayer.kt
+++ app/src/main/java/com/example/movopfy/network/anilibria/models/AnilibriaPlayer.kt
@@ -2,9 +2,10 @@ package com.example.movopfy.network.anilibria.models
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
 
 @Serializable
 data class AnilibriaPlayer(
     @SerialName("episodes") val episodes: AnilibriaEpisodes?,
-    @SerialName("list") val list: AnilibriaEpisodesList?
+    @SerialName("list") val list: JsonElement?
 )
