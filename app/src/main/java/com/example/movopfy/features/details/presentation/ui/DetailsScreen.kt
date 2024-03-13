8d5b2da48726e9498945aaea640b372c5512e55f
siarheihaurylau <sgavrilov387@gmail.com>
2024-03-13 16:14:18 +0300

    detail screen: added episodes list, removed top bar

--- app/src/main/java/com/example/movopfy/features/details/presentation/ui/DetailsScreen.kt
+++ app/src/main/java/com/example/movopfy/features/details/presentation/ui/DetailsScreen.kt
@@ -1,8 +1,11 @@
 package com.example.movopfy.features.details.presentation.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,13 +19,15 @@ import coil.compose.AsyncImage
 import com.example.movopfy.common.constants.API_CATEGORY_ANILIBRIA
 import com.example.movopfy.common.constants.API_CATEGORY_KINOPOISK
 import com.example.movopfy.common.extensions.getSmallImageUrl
+import com.example.movopfy.common.mappers.anilibria.mapToAnilibriaEpisodesList
 import com.example.movopfy.features.details.presentation.viewmodel.DetailsViewModel
 import com.example.movopfy.uiComponents.components.ProgressBarLoading
+import com.example.movopfy.uiComponents.navigation.Screen
+import com.example.movopfy.uiComponents.theme.dimensions
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun DetailsScreen(
-    modifier: Modifier = Modifier,
     viewModel: DetailsViewModel = koinViewModel(),
     id: Int,
     category: String,
@@ -37,7 +42,7 @@ fun DetailsScreen(
         }
     }
 
-    Surface(modifier = modifier) {
+    Surface {
         when (val state = uiState) {
             is DetailsViewModel.DetailsUiState.Loading -> {
                 ProgressBarLoading()
@@ -92,6 +97,34 @@ fun DetailsScreen(
                         DescriptionText(
                             description = description
                         )
+                    }
+
+                    item {
+                        CustomDivider()
+                    }
+
+                    item {
+                        Box(
+                            modifier = Modifier
+                                .height(height = MaterialTheme.dimensions.paddingTop)
+                                .fillMaxWidth()
+                        )
+                    }
+
+                    itemsIndexed(mapToAnilibriaEpisodesList(state.anilibriaTitle?.player?.list).reversed()) { index, item ->
+                        Episode(
+                            episode = item.episode,
+                            name = item.name ?: "",
+                            color = if (index % 2 == 0) MaterialTheme.colorScheme.onSecondary
+                            else MaterialTheme.colorScheme.background
+                        ) {
+                            navController.navigate(
+                                Screen.Player.passId(
+                                    id = id,
+                                    episode = item.episode?.minus(1) ?: 0
+                                )
+                            )
+                        }
                     }
                 }
             }
