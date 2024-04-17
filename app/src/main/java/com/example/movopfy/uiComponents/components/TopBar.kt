91a8e11f09a427ea9f5ae2fb0f865def0a4f2a21
siarheihaurylau <sgavrilov387@gmail.com>
2024-04-17 10:35:15 +0300

    top bar: remove icon on home

--- app/src/main/java/com/example/movopfy/uiComponents/components/TopBar.kt
+++ app/src/main/java/com/example/movopfy/uiComponents/components/TopBar.kt
@@ -1,6 +1,5 @@
 package com.example.movopfy.uiComponents.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -15,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -48,16 +46,6 @@ fun TopBar(isBackEnabled: Boolean, navController: NavController) {
                         navController.popBackStack()
                     },
                 tint = MaterialTheme.colorScheme.onPrimary
-            )
-        } else {
-            Image(
-                painter = painterResource(id = R.drawable.ic_top_bar),
-                contentDescription = null,
-                modifier = Modifier
-                    .shadow(
-                        elevation = 30.dp,
-                        shape = MaterialTheme.shapes.medium
-                    )
             )
         }
 
