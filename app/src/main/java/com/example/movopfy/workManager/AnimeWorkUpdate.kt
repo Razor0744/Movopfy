3147f424abfe6cff3bf979c8c896aa69881f7611
siarheihaurylau <sgavrilov387@gmail.com>
2024-06-27 11:23:46 +0300

    work manager

---
+++ app/src/main/java/com/example/movopfy/workManager/AnimeWorkUpdate.kt
@@ -0,0 +1,41 @@
+package com.example.movopfy.workManager
+
+import android.content.Context
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import java.util.Calendar
+import java.util.concurrent.TimeUnit
+
+class AnimeWorkUpdate {
+
+    fun startWork(context: Context) {
+        val targetTime = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 1)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+            add(Calendar.DAY_OF_MONTH, 1)
+        }
+
+        val delay = targetTime.timeInMillis - Calendar.getInstance().timeInMillis
+
+        val constrains = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val workRequest = PeriodicWorkRequestBuilder<AnimeWorker>(24, TimeUnit.HOURS)
+            .setConstraints(constrains)
+            .setInitialDelay(delay, TimeUnit.MINUTES)
+            .build()
+
+        WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+            "anime",
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
+        )
+    }
+}
